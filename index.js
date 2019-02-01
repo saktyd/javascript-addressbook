daf34818505e40ac476571f3a6bf3b93dbda9bd0
Sakti Dewantoro <saktyd@gmail.com>
2019-02-01 10:23:17 +0700

    improve code on function add and delete address book

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
+var id = 3;
 var address_book = [
   {
+    id: 1,
     name: 'Sakti',
     address: 'Bandung',
     noHp: '01234'
   },
   {
+    id: 2,
     name: 'Bara',
     address: 'Bandung',
     noHp: '14045'
   },
   {
+    id: 3,
     name: 'Fajrin',
-    address: 'Wamena'
+    address: 'Wamena',
+    noHp: '14045'
   }
 ];
 
 function add_address_book(name, address, noHp) {
+  id = id + 1;
   address_book.push({
+    id,
     name,
     address,
     noHp
   });
 }
 
-function delete_Address_book(name) {
-  let someuser = address_book.filter(
-    item => item.name.toLowerCase() !== name.toLowerCase()
-  );
+function delete_Address_book(noID) {
+  let someuser = address_book.filter(item => item.id !== noID);
   address_book = someuser;
   return address_book;
 }
